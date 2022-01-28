2a41f4fcfed9416bee3a5b4f5ebf95ecedb0520e
DaveyJH <daveyj.dh@googlemail.com>
2022-01-28 16:58:41 +0000

    feat: Following tutorial with extras added

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,3 +2,26 @@ document.addEventListener('DOMContentLoaded', function() {
   let sidenav = document.querySelectorAll('.sidenav');
   M.Sidenav.init(sidenav);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  let modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  let datePicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datePicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {done: "Select"}
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  let collapsibles = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsibles);
+});
